96965e182d4b1b3087c8070eca1c20c1642c3832
Alexander <alexander.c1989@gmail.com>
2022-07-17 10:36:15 -0400

    bcrypt and auth with jwt

--- server/database.sql
+++ server/database.sql
@@ -8,8 +8,8 @@ CREATE TABLE todo(
 CREATE TABLE users(
     user_id SERIAL PRIMARY KEY,
     user_name VARCHAR(255) NOT NULL,
-    use_email VARCHAR(255) NOT NULL,
-    use_password VARCHAR(255) NOT NULL
+    user_email VARCHAR(255) NOT NULL,
+    user_password VARCHAR(255) NOT NULL
 );
 
 INSERT INTO users (user_name, user_email, user_password) VALUES ('alex', 'alex@gmail.com', 'alex')
